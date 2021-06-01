ed1d2961fb3ddd332a2e7fef5b4d27bc9892d579
HornyFerret <ombro.de.morto@gmail.com>
2021-06-01 15:54:07 +0300

    Добавьте файлы проекта.

---
+++ Масивы/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Масивы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[] { 6, 9, 6, 1, 9, 6, 9 };
+
+            Console.WriteLine("Задание 1");
+
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.WriteLine("Задание 2");
+           
+            int n = num.Length;
+            int k = n / 2;
+            int temp;
+            for (int i = 0; i < k; i++)
+            {
+                temp = num[i];
+                num[i] = num[n - i - 1];
+                num[n - i - 1] = temp;
+
+            }
+            foreach (int i in num) 
+            {
+                Console.Write($" {i} \t");
+            }
+           
+            Console.WriteLine(" ");
+            Console.WriteLine("Задание 3");
+
+            List<int> set = new List<int>();
+
+            for (int f = 0; f < num.Length; f++) 
+            {
+                if (!set.Contains(num[f]))
+                    set.Add(num[f]);
+            }
+            foreach (int f in set)
+            {
+                Console.WriteLine($" {f} \t");
+            }
+            //либо ShowArray(f); как замена методу foreach
+
+           
+
+        }
+
+    }
+    
+    
+
+}
---
+++ Массивы 2/Массивы 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Массивы_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
